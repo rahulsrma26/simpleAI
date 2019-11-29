7912d7f7dcb9d7f8d94c8d4fb8d4a3338367ca92
Rahul Sharma <rahulsrma26@gmail.com>
2019-11-29 14:34:26 +0530

    using ompint instead of size_t (breaking in msvc2019)

--- libsnn/src/activator/leaky_relu.cpp
+++ libsnn/src/activator/leaky_relu.cpp
@@ -13,7 +13,7 @@ tensor<real> leaky_relu::f(const tensor<real>& t) const {
     tensor<real> r(t.get_shape());
     const auto n = t.size();
 #pragma omp parallel for if (n >= OPENMP_SMALL_THRESHOLD)
-    for (size_t i = 0; i < n; i++)
+    for (ompint i = 0; i < n; i++)
         r[i] = t[i] > 0 ? t[i] : leak_m * t[i];
     return r;
 }
@@ -22,7 +22,7 @@ tensor<real> leaky_relu::df(const tensor<real>& t) const {
     tensor<real> r(t.get_shape());
     const auto n = t.size();
 #pragma omp parallel for if (n >= OPENMP_SMALL_THRESHOLD)
-    for (size_t i = 0; i < n; i++)
+    for (ompint i = 0; i < n; i++)
         r[i] = t[i] > 0 ? 1 : leak_m;
     return r;
 }
